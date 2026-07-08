655df68e1980b191998bf88f3649dd1ffa5ebc15
dovlym <74021197+dovlym@users.noreply.github.com>
2026-07-08 16:51:41 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
           Frontend Mentor.
         </a>
         Coded by
-       <a href="https://github.com/dovlym" target="_blank">Dusvy_M</a>
+       <a href="https://github.com/dovlym" target="_blank">Dusvy M</a>
       </footer>
     </>
   );
